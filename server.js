9abf48ee19fa391fce3890948175c41811d439e4
andresochoa91 <andres_ochoa91@hotmail.com>
2021-08-10 22:05:19 -0700

    Updates docker

--- server.js
+++ server.js
@@ -23,54 +23,60 @@ const db = knex({
 app.use(express.urlencoded({extended: false}));
 app.use(express.json())
 
-
 app.get('/', (req, res) => {
-  db.select('*').from('users').then(data => {
-    res.status(200).send(data);
-  });
+    res.send("hello amigos");
 });
 
-app.get('/:id', (req, res) => {
-  try {
-    db.select('*').from('users').where('id', req.params.id).then(data => {
-      if (data.length) {
-        res.status(200).send(data[0]);
-      } else {
-        res.status(404).send({ error: "user not found" });
-      }
-    });
-  } catch {
-    res.status(404).send({ error: "not found" });
-  }
-});
 
-app.post('/', (req, res) => {
-  db('users')
-  .insert(req.body)
-  .returning('*')
-  .then(data => {
-    res.send(data[0]);
-  })
-  .catch(console.error);  
-});
+// app.get('/', (req, res) => {
+//   db.select('*').from('users').then(data => {
+//     res.status(200).send(data);
+//   });
+// });
 
-app.put('/:id', (req, res) => {
-  db('users')
-  .where('id', req.params.id)
-  .update(req.body)
-  .returning('*')
-  .then(data => {
-    res.send(data[0]);
-  })
-  .catch(console.error);
-});
+// app.get('/:id', (req, res) => {
+//   try {
+//     db.select('*').from('users').where('id', req.params.id).then(data => {
+//       if (data.length) {
+//         res.status(200).send(data[0]);
+//       } else {
+//         res.status(404).send({ error: "user not found" });
+//       }
+//     });
+//   } catch {
+//     res.status(404).send({ error: "not found" });
+//   }
+// });
 
-app.delete('/:id', (req, res) => {
-  console.log(req.params.id)
-  db('users')
-  .where('id', req.params.id)
-  .del()
-  .then(() => res.send({ 'success': 'user deleted' }));
-});
+// app.post('/', (req, res) => {
+//   db('users')
+//   .insert(req.body)
+//   .returning('*')
+//   .then(data => {
+//     res.send(data[0]);
+//   })
+//   .catch(console.error);  
+// });
 
-app.listen(3000);
+// app.put('/:id', (req, res) => {
+//   db('users')
+//   .where('id', req.params.id)
+//   .update(req.body)
+//   .returning('*')
+//   .then(data => {
+//     res.send(data[0]);
+//   })
+//   .catch(console.error);
+// });
+
+// app.delete('/:id', (req, res) => {
+//   console.log(req.params.id)
+//   db('users')
+//   .where('id', req.params.id)
+//   .del()
+//   .then(() => res.send({ 'success': 'user deleted' }));
+// });
+
+app.listen(3000, () => {
+  console.log("running in port 3000");
+});
